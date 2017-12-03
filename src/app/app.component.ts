f8cb199ba9f97eac97f2dc17d48e4d4db1710b9c
Jarek Ratajski <jratajski@gmail.com>
2017-12-03 14:00:32 -0800

    LEKCJA4: koniec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  time = 'xyz';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/api/time', {responseType: 'text'}).subscribe(data => {
+      this.time = data;
+    });
+  }
 }
