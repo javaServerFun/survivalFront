0ea53e67f42b1d2de1237c10aa48714d8c460b3d
Jarek Ratajski <jratajski@gmail.com>
2017-12-09 08:12:13 -0800

    LEKCJA6: Message added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 
+interface Message {
+  content: string;
+  author: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
   time = 'xyz';
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get('/api/time', {responseType: 'text'}).subscribe(data => {
       this.time = data;
     });
+
   }
 }
